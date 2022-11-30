e9ecd386ca316b61521ce5df979d4b92bb91b53b
coderzabhiraj <118661389+coderzabhiraj@users.noreply.github.com>
2022-12-01 00:18:32 +0530

    variable
    variables in python

---
+++ class18-VARIABLE -cipherScools.py
@@ -0,0 +1,12 @@
+number1 = 2
+print(number1)
+number1 = 4
+print(number1)
+# string number
+name = "abhiraj"
+print (name)
+name = (120)
+print (name)
+_name ="abhiraj"
+user_one_name ="abhiraj" # snake case writing
+userOneName ="abhiraj" #camel case writing
