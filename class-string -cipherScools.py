0dd5ee44ba5b5d3fb27384b4ff0f1c9a883e6010
coderzabhiraj <118661389+coderzabhiraj@users.noreply.github.com>
2022-12-01 01:59:03 +0530

    class string

---
+++ class-string -cipherScools.py
@@ -0,0 +1,7 @@
+first_name = "abhiraj"
+last_name = "ojha"
+full_name = first_name + "" + last_name 
+print (full_name)
+print (first_name + "3")
+print (first_name + str(3))
+print (first_name * 3)
